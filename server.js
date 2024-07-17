e1553e330b4b7e0a35f76a4c7c26c92d40ba42d9
ali-lutfalla <ali,latfalla@gmail.com>
2024-07-17 20:54:15 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,82 @@
+const express  = require('express');
+
+const app = express();
+
+app.get('/greetings/:username', (req , res) => {
+    res.send(`Hello there, ${req.params.username}`);
+})
+
+app.get('/roll/:numberInput', (req ,res) => {
+    const number = parseInt(req.params.numberInput);
+    if (isNaN(number)){
+        res.send('You must specify a number.');
+    } else {
+        res.send(`You rolled a ${Math.floor(Math.random() * (number - 0 + 1) + 0)}`);
+    }
+})
+
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+app.get('/collectibles/:index', (req, res) => {
+    const index = req.params.index;
+    if (index >= collectibles.length || index < 0){
+        res.send('This item is not yet in stock. Check back soon!');
+    } else {
+        res.send(`So, you want the ${collectibles[index].name}? For ${collectibles[index].price} it can be yours!`);
+    }
+})
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+app.get('/shoes',(req, res) => {
+    let printArray = shoes;
+    let minPrice = undefined;
+    let maxPrice = undefined;
+    let type;
+    if (Object.keys(req.query).length === 0) {
+        res.send(shoes);
+    }
+    for (const [key, value] of Object.entries(req.query)) {
+        if (key === 'min-price'){
+             minPrice = parseInt(value);
+        } 
+        if (key === 'max-price'){
+             maxPrice = parseInt(value);
+        }
+        if (key === 'type'){
+             type = req.query.type;
+        }
+      }
+    if (minPrice !== undefined){
+        printArray = printArray.filter((element) => {
+            return element.price > minPrice;
+        });
+    }
+    if (maxPrice !== undefined){
+        printArray = printArray.filter((element) => {
+            return element.price < maxPrice;
+        });
+    }
+    if (type !== undefined){
+        printArray = printArray.filter((element) => {
+            return element.type === type;
+        });
+    }
+    res.send(printArray);
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+  })
